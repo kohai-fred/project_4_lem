afb208892b510533da39effc3a610f7a46c13230
kohai-fred <frederick.vu.pro@gmail.com>
2022-09-12 15:53:37 +0200

    Setting up react-router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./Components/NavBar";
-import Login from "./views/Login";
 import { store } from "./features/store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "./Components/NavBar";
+import Login from "./views/Login";
+import Homepage from "./views/Homepage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
         <Navbar />
-        <Login />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="homepage" element={<Homepage />} />
+            </Routes>
+        </BrowserRouter>
     </Provider>
 );

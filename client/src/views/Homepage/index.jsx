e79a622312414431f14f18f3375176e479c5c8e0
kohai-fred <frederick.vu.pro@gmail.com>
2022-09-13 09:02:15 +0200

    Display a random user WIP

--- client/src/views/Homepage/index.jsx
+++ client/src/views/Homepage/index.jsx
@@ -1,23 +1,35 @@
-import { Typography } from "@mui/material";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
+import ProfileCard from "../../Components/ProfileCard";
 import getRandomUser from "../../services/fetch/randomUser";
+import { Button, Stack, Typography } from "@mui/material";
+import PanToolIcon from "@mui/icons-material/PanTool";
 
 const Homepage = () => {
     const [user, setUser] = useState(null);
     const [errorMessage, setErrorMessage] = useState(null);
-    useMemo(async () => {
+    const randomUser = async () => {
         const [data, error] = await getRandomUser();
         if (error) return setErrorMessage(error);
         setUser(data);
+    };
+    useMemo(() => {
+        randomUser();
     }, []);
+
     return (
         <div>
-            {errorMessage ? (
+            {errorMessage && (
                 <Typography variant="h5" textAlign={"center"}>
                     {errorMessage}
                 </Typography>
-            ) : (
-                <div>Hello {user?.firstname}</div>
+            )}
+            {user && (
+                <Stack m="auto" id="TOTO">
+                    <ProfileCard user={user} />
+                    <Button variant="contained" startIcon={<PanToolIcon />} onClick={randomUser}>
+                        Dire bonjour à quelqu'un d'autre
+                    </Button>
+                </Stack>
             )}
         </div>
     );

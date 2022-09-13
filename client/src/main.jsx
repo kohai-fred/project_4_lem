c0c837fbb6c5fce93f9eef07e1447067fa2e3ff2
kohai-fred <frederick.vu.pro@gmail.com>
2022-09-13 18:11:59 +0200

    Feat add minimum global css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,15 +7,21 @@ import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Components/NavBar";
 import Login from "./views/Login";
 import Homepage from "./views/Homepage";
+import Collaborateurs from "./views/Collaborateurs";
+import "./main.css";
+import { Box } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
         <BrowserRouter>
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="homepage" element={<Homepage />} />
-            </Routes>
+            <Box p={4}>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="homepage" element={<Homepage />} />
+                    <Route path="collaborateurs" element={<Collaborateurs />} />
+                </Routes>
+            </Box>
         </BrowserRouter>
     </Provider>
 );

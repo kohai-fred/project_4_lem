1b12e5e11e8b0f8265f57bbecc16bbfca93446f2
kohai-fred <frederick.vu.pro@gmail.com>
2022-09-15 00:25:58 +0200

    Fix: missing category

--- client/src/services/utils/search/reduceByCollaboratorsCategory.js
+++ client/src/services/utils/search/reduceByCollaboratorsCategory.js
@@ -1,4 +1,4 @@
-export const reduceByCollaboratorsCategory = (collaborators) => {
+export const reduceByCollaboratorsCategory = (collaborators, category) => {
     const data = collaborators.reduce((acc, curr) => {
         if (curr.service === category) acc.push(curr);
         return acc;

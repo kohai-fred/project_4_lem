944a842fe08c6ca025ebae3896740142f5b5b675
kohai-fred <frederick.vu.pro@gmail.com>
2022-09-18 02:25:15 +0200

    Fix:  behavior protected route formulaire token or id user is granted

--- client/src/Components/Protected/Protected.jsx
+++ client/src/Components/Protected/Protected.jsx
@@ -1,31 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { setUser } from "../../features/user";
-import axiosInstance from "../../services/fetch/axiosInstance";
+import { getUser } from "../../services/fetch";
 import { getLocalStorage } from "../../services/utils/getLocalStorage";
 
 const Protected = ({ children }) => {
     const localSto = getLocalStorage();
     const navigate = useNavigate();
+    const { id } = useParams();
+    const location = useLocation();
+
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user.value);
     const [isConnected, setIsConnected] = useState(false);
 
+    const gettingUser = async () => {
+        const [data, error] = await getUser(localSto.id);
+        if (error) return navigate("/");
+        dispatch(setUser({ ...data, isAdmin: localSto.isAdmin }));
+        setIsConnected(true);
+    };
+
     useEffect(() => {
         if (!localSto) return navigate("/");
-
+        if (user && location.pathname === "/") return navigate("/");
         if (user) return setIsConnected(true);
-        const getUser = async () => {
-            const [data, error] = await axiosInstance(`/collaborateurs/${localSto.id}`);
-            if (error) return navigate("/");
-            dispatch(setUser({ ...data, isAdmin: localSto.isAdmin }));
-            setIsConnected(true);
-        };
-        getUser();
-    }, []);
+
+        gettingUser();
+    }, [user]);
+
+    if (!localSto && location.pathname === "/") return <>{children}</>;
 
     if (!isConnected) return;
+
+    if (user && !user.isAdmin && location.pathname.includes("formulaire") && user.id !== id)
+        return navigate("/homepage");
+
     return <>{children}</>;
 };
 
